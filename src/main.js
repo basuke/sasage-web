7e53971b80aeaadf430cac02a20bdf4976201f22
Basuke Suzuki <basuke@mac.com>
2020-08-02 12:14:19 -0700

    Added Svelte build pipeline and move contents into App

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import App from './App.svelte';
+
+document.addEventListener('DOMContentLoaded', function() {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+        let app = firebase.app();
+        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+        console.log(`Firebase SDK loaded with ${features.join(', ')}`);
+
+        const target = document.querySelector('#app');
+        // remove all contents.
+        Array.from(target.childNodes).forEach(node => node.parentNode.removeChild(node));
+        // install svelte app.
+        new App({
+            target,
+            props: {
+                name: 'Svelte',
+                app,
+            }
+        });
+    } catch (e) {
+        console.error(`Error loading the Firebase SDK, check the console: ${e}`);
+    }
+});
+
+export default app;
